cfdcafa5cb155451a9085562cdb0b25eac3572d7
Rahwa Gebreyesus <rahwa3113@gmail.com>
2025-02-11 00:31:33 +0300

    third commiting

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ const app = express();
 
 app.use(cors());
 let corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "https://finalprojectfrontend-9jl5.onrender.com/",
+    "http://localhost:5173",
+  ],
   credentials: true,
 };
 app.use(express.json());
